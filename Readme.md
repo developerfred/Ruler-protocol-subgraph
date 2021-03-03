1616855899010fb1dd1f85e91355390b1d646cbc
@codingsh <codingsh@pm.me>
2021-03-03 18:18:43 -0300

    feat(subgraph): create Ruler Protocol subgraph

---
+++ Readme.md
@@ -0,0 +1,15 @@
+### Ruler Protocol Subgraph
+
+#### Contracts 
+
+- [RulerCore](https://etherscan.io/address/0xF19f4490A7fCCfEf2DaB8199ACDB2Dc1B9027C18)
+- [RulerMining](0x3423c8Af3a95D9FEE7Ec06c4e0E905D4fd559F89)
+- [RulerToken](https://etherscan.io/address/0x2aeccb42482cc64e087b6d2e5da39f5a7a7001f8)
+- [RulerZap](https://etherscan.io/address/0xBd856654bA2B6DC12C55A7Bff9730eD5b056b1b1)
+- [RulerOracle](https://etherscan.io/address/0x1447Db893bC4f6767460AD72359deAd840339c6a)
+- [RulerSushiLP](https://etherscan.io/address/0xb1EECFea192907fC4bF9c4CE99aC07186075FC51) 
+
+---
+
+- [codingsh](https://twitter.com/codingsh)
+- [buy a coffe to codingsh.eth](https://etherscan.io/address/codingsh.eth)
--- src/mapping.ts
+++ src/mapping.ts
@@ -5,64 +5,83 @@ import {
   OwnershipTransferred,
   Transfer
 } from "../generated/RulerToken/RulerToken"
-import { ExampleEntity } from "../generated/schema"
+import { Swap, Mint, Burn, Sync } from '../generated/RulerSushiLP/RulerSushiLP'
+import { Deposit, Withdraw, PausedStatusUpdated } from '../generated/RulerMining/RulerMining'
+import { RulerTransfer, RulerOwnershipTransferred, RulerSwap, RulerMint, RulerBurn, RulerSync, RulerDeposit, RulerWithdraw, RulerPausedStatusUpdated } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {
+  let OwnershipTransfer = new RulerOwnershipTransferred(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.CAP(...)
-  // - contract.DOMAIN_SEPARATOR(...)
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.decimals(...)
-  // - contract.decreaseAllowance(...)
-  // - contract.increaseAllowance(...)
-  // - contract.name(...)
-  // - contract.nonces(...)
-  // - contract.owner(...)
-  // - contract.symbol(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
+  OwnershipTransfer.previousOwner = event.params.previousOwner;
+  OwnershipTransfer.previousOwner = event.params.newOwner;
+  OwnershipTransfer.save()
 }
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+export function handleTransfer(event: Transfer): void {
+  
+  let rulerToken = new RulerTransfer(event.transaction.from.toHex())
+  rulerToken.from = event.params.from;
+  rulerToken.to = event.params.to;
+  rulerToken.value = event.params.value;
 
-export function handleTransfer(event: Transfer): void {}
+  rulerToken.save()
+}
+
+export function handleSwap(event: Swap): void {
+  
+  let swap = new RulerSwap(event.transaction.from.toHex())
+  swap.sender = event.params.sender;
+  swap.amount0In = event.params.amount0In;
+  swap.amount1In = event.params.amount1In;
+  swap.amount0Out = event.params.amount0Out;
+  swap.amount1Out = event.params.amount1Out;
+  swap.to = event.params.to;
+
+  swap.save()
+}
+
+export function handleMint(event: Mint): void {
+  
+  let mint = new RulerMint(event.transaction.from.toHex())
+  mint.sender = event.params.sender;
+  mint.amount0 = event.params.amount0;
+  mint.amount1 = event.params.amount1;
+
+  mint.save();
+}
+
+export function handleBurn(event: Burn): void {
+  
+  let burn = new RulerBurn(event.transaction.from.toHex())
+  burn.amount0 = event.params.amount0;
+  burn.amount1 = event.params.amount1;
+
+  burn.save();
+}
+
+export function handleSync(event: Sync): void {
+  
+  let sync = new RulerSync(event.transaction.from.toHex())
+  sync.reserve0 = event.params.reserve0;
+  sync.reserve1 = event.params.reserve1;
+
+  sync.save();
+}
+
+export function handleWithdraw(event: Withdraw): void {
+  
+  let whitdraw = new RulerWithdraw(event.transaction.from.toHex())
+  whitdraw._lpToken = event.params.lpToken
+  whitdraw._amount = event.params.amount
+
+  whitdraw.save()
+}
+
+export function handleDeposit(event: Deposit): void {
+  
+  let deposit = new RulerDeposit(event.transaction.from.toHex())
+  deposit.lpToken = event.params.lpToken
+  deposit.amount = event.params.amount
+
+  deposit.save()
+}
